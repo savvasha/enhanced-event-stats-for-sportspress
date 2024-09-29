1d67fb4d4d7266063d8868fb8f7b02e417eab159
savvasha <savvasha@hotmail.com>
2024-09-29 13:25:18 +0300

    Initial commit

---
+++ enhanced-event-stats-for-sportspress.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Plugin Name: Enhanced Event Stats for SportsPress
+ * Description: Show extra stats and performances of the participating teams of an event.
+ * Version: 1.0
+ * Author: Savvas
+ * Author URI: https://profiles.wordpress.org/savvasha/
+ * Requires at least: 5.3
+ * Requires PHP: 7.4
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl.html
+ *
+ * @package enhanced-event-stats-for-sportspress
+ * @category Core
+ * @author savvasha
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+// Define constants.
+if ( ! defined( 'EESSP_PLUGIN_BASE' ) ) {
+	define( 'EESSP_PLUGIN_BASE', plugin_basename( __FILE__ ) );
+}
+if ( ! defined( 'EESSP_PLUGIN_DIR' ) ) {
+	define( 'EESSP_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+}
+
+if ( ! defined( 'EESSP_PLUGIN_URL' ) ) {
+	define( 'EESSP_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+}
+
+// Hooks.
+add_filter( 'sportspress_event_templates', 'eessp_event_templates' );
+//add_filter( 'sportspress_player_settings', 'fnlstats_add_settings' );
+
+add_action( 'wp_enqueue_scripts', 'eessp_css_styles_scripts' );
+
+/**
+ * Add css styles and scripts to player pages only.
+ *
+ * @return void
+ */
+function eessp_css_styles_scripts() {
+	wp_register_style( 'eessp-styles', EESSP_PLUGIN_URL . 'assets/css/eessp_style.css' );
+	if ( get_post_type( get_the_ID() ) == 'sp_event' ) {
+		wp_enqueue_style( 'eessp-styles' );
+	}
+}
+
+/**
+ * Add templates to event layout.
+ *
+ * @return array
+ */
+function eessp_event_templates( $templates = array() ) {
+	$templates['eessp_form_guide'] = array(
+		'title'   => __( 'Form Guide', 'eessp' ),
+		'option'  => 'sportspress_event_show_eessp_form_guide',
+		'action'  => 'output_eessp_form_guide',
+		'default' => 'yes',
+	);
+	return $templates;
+}
+
+/**
+ * Output eessp form guide template.
+ *
+ * @access public
+ * @return void
+ */
+function output_eessp_form_guide() {
+	$teams = get_post_meta( get_the_ID(),'sp_team' );
+	foreach ( $teams as $team ) {
+		sp_get_template( 'event-list.php', array(
+			'title' => get_the_title( $team ),
+			'show_title' => true,
+			'team' => $team,
+			'date_before' => get_post_time('Y-m-d', true),
+			'title_format' => 'homeaway',
+			'time_format' => 'separate',
+			'columns' => apply_filters( 'eessp_form_guide_columns', array( 'event', 'time', 'results' ) ),
+			'order' => 'DESC',
+			'hide_if_empty' => true,
+		) );
+	}
+}
---
+++ readme.txt
@@ -0,0 +1,31 @@
+=== Enhanced Event Stats for SportsPress ===
+Contributors: savvasha
+Tags: sportspress, teams, season, events, statistics
+Requires at least: 5.3
+Tested up to: 6.5
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
+
+Show the extra stats and performances of the participating teams of an event.
+
+== Description ==
+
+This plugin is an extension for [SportsPress](https://wordpress.org/plugins/sportspress/). Give your guests the option to see extra stats about the participating teams of an event.
+
+== Installation ==
+
+1.  Install and activate Enhanced Event Stats for SportsPress plugin
+2.  Enjoy!
+
+= Credits =
+
+* [Themeboy](https://www.themeboy.com/)
+
+== Screenshots ==
+
+== Changelog ==
+
+= 1.0 =
+
+* First Release!
